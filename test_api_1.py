24b265c96741d66295b03e21f216318226bd5cf4
Pichkobiy <galachevairina@gmail.com>
2024-11-08 07:36:25 +0300

    Tests

--- test_api_1.py
+++ test_api_1.py
@@ -6,12 +6,44 @@ headers = {
 "Authorization": f"Bearer {token}",
 "Content-Type": "application/json"
 }
+body = {
+    "searchPhrase":"стивен кинг",
+    "resultCount":793
+}
 
-def test_get():
-    url = 'https://www.chitai-gorod.ru/'
+def test_post():
+    url = 'https://web-gate.chitai-gorod.ru/api/v2/search/results'
     response = requests.get(url, headers=headers)
 
     assert response.status_code == 204
     assert 'content' in response.json()
 
-    
+def test_get():
+    url = 'https://web-gate.chitai-gorod.ru/api/v2/categories'
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200   
+
+def test_get_2():
+    url = 'https://web-gate.chitai-gorod.ru/api/v2/products/slug/fairy-tale-3027523'
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200   
+
+body = {
+    "id":134243402,
+    "quantity":2
+}
+
+def test_put():
+    url = 'https://web-gate.chitai-gorod.ru/api/v2/cart'
+    response = requests.put(url, headers=headers, json=body)
+
+    assert response.status_code == 200
+    assert 'content' in response.json()    
+
+def test_delete():
+    url = 'https://web-gate.chitai-gorod.ru/api/v2/cart/product/134420243'
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 404    
--- test_ui_1.py
+++ test_ui_1.py
@@ -9,4 +9,33 @@ driver.get("https://www.chitai-gorod.ru/")
 
 search_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.CLASS_NAME, 'header-search'))
 search_button.click()
+
+
+driver = webdriver.Firefox()
+driver.get("https://www.chitai-gorod.ru/")
+
+search_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.CLASS_NAME, 'header-search'))
+search_button.click()
+
+
+driver = webdriver.Edge()
+driver.get("https://www.chitai-gorod.ru/")
+
+search_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.CLASS_NAME, 'header-search'))
+search_button.click()
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.chitai-gorod.ru/")
+search_button = driver.find_element(By.CLASS_NAME, "search-button")
+
+search_button.send_keys("Квесты")
+search_button = WebDriverWait(15)
+search_button.click()
+
+driver = webdriver.Chrome()
+driver.get("https://www.chitai-gorod.ru/")
+driver.set_window_size(640, 460)
+driver = WebDriverWait(20)
+
 driver.quit()
